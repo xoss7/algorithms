67056fab32adf1b16b2afa012b724167068d1c84
codefun <codeforfun77@gmail.com>
2024-06-13 09:12:28 +0000

    Implementation of Stack

---
+++ Chapter 3 - Data Structures/stack.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct stack {
+    int item;
+    struct stack* next;
+} stack;
+
+
+void push(int item, stack** top) {
+    stack* node = (stack*)malloc(sizeof(stack));
+    node->item = item;
+    node->next = *top;
+    *top = node;
+}
+
+
+stack* pop(stack** top) {
+    stack* node = *top;
+    *top = (*top)->next;
+    return node;
+}
+
+void printStack(stack* top) {
+    while (top != NULL) {
+        printf("%d -> ", top->item);
+        top = top->next;
+    }
+    printf("NULL\n");
+}
